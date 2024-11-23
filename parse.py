e1a7d8623ceac03a666157b508f48a95188a329e
gdfella-226 <kosorev.daniel@gmail.com>
2024-11-23 15:09:02 +0300

    fix message encodig

--- parse.py
+++ parse.py
@@ -1,11 +1,12 @@
 from telethon.sync import TelegramClient
 from json import dump
+import codecs
 
 def get_last_message(api_id: int, api_hash: str, links: list):
     with TelegramClient('parse_session', api_id, api_hash) as client:
-        try:
-            messages = []
-            for link in links:
+        messages = []
+        for link in links:
+            try:
                 print(f"Proccesed {links.index(link)}/{len(links)}")
                 chat_username = link.split('/')[-2]
                 chat = client.get_entity(chat_username)
@@ -13,17 +14,18 @@ def get_last_message(api_id: int, api_hash: str, links: list):
                 if last_message and last_message[0].sender and last_message[0].sender.username:
                     message_data = {
                         "text": last_message[0].message,
-                        "user": last_message[0].sender.username
+                        "user": last_message[0].sender.username,
+                        "chat": link
                     }
                     print(f"Found message from {message_data['user']}")
-                    messages.append( message_data)
-            return {"messages": messages}
-        except Exception as e:
-            return None
+                    messages.append(message_data)
+            except Exception as e:
+                continue
+        return {"messages": messages}
 
 
 def get_api_params():
-    with open('./data/config', 'r') as cfg_file:
+    with open('./config/config', 'r') as cfg_file:
         lines = cfg_file.readlines()
         for line in lines:
             if line.split('=')[0] == 'api_hash':
@@ -39,8 +41,8 @@ if __name__ == "__main__":
     api_id = params['api_id']
     api_hash = params['api_hash']
 
-    with open('./data/links', 'r') as links_file_r:
+    with open('./config/links', 'r') as links_file_r:
         links = links_file_r.readlines()
         res = get_last_message(api_id, api_hash, links)
-    with open("messages.json", "w") as outfile:
-        dump(res, outfile)
+    with codecs.open("messages.json", "w", "utf-16") as outfile:
+        dump(res, outfile, ensure_ascii=False)
